ef5dfb238104fd05b92acd228472f943dd1166d8
rémi <remirobert33530@gmail.com>
2014-12-26 18:23:13 +0100

    add admin account creation at start

--- testApp/src/main/java/com/springapp/mvc/StartApp.java
+++ testApp/src/main/java/com/springapp/mvc/StartApp.java
@@ -5,10 +5,35 @@ package com.springapp.mvc;
  */
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
+import com.springapp.mvc.Model.User;
+import com.springapp.mvc.Model.Account;
+import com.springapp.mvc.Service.UserService;
+import com.springapp.mvc.Service.AccountService;
 
 @SuppressWarnings("serial")
 public class StartApp extends HttpServlet
 {
+
+    private void initAdiministratorAccount() {
+        UserService userService = new UserService();
+
+        if (userService.isUserExist("root") != null) {
+            return;
+        }
+
+        User newAdmin = new User("admin", "root", "root", 2);
+        userService.addUser(newAdmin);
+
+        String idCurrentClient = null;
+        if ((idCurrentClient = userService.getIdUser(newAdmin)) == null)
+            return;
+
+        Account newAccount = new Account(idCurrentClient, "root");
+        AccountService accountService = new AccountService();
+        accountService.createAccount(newAccount);
+    }
+
     public void init() throws ServletException {
+        this.initAdiministratorAccount();
     }
 }
